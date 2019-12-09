3518d02fb8c87f99df6ae95a5c8c6f202e29524d
sony <sony@localhost.localdomain>
2019-12-09 15:37:56 +0000

    Level handler

--- fibonacci/src/App.js
+++ fibonacci/src/App.js
@@ -5,8 +5,8 @@ import Footer from './Footer'
 
 function App() {
   
-  const [nValue, nValueHandler] = useState("3");
-  const [answer, answerHandler] = useState("2");
+  const [nValue, setnValue] = useState("3");
+  const [answer, setAnswer] = useState("2");
   
   const fiboCalculator = (n)=>{
     if (n <= 2) return 1;
@@ -15,18 +15,40 @@ function App() {
       f[i] = f[i-1] + f[i-2];
     }
     let correctAnswer = f[f.length-1];
-    if (correctAnswer.toString() === answer.toString()) 
-    alert('Woowow ' + correctAnswer +" is correct, genius !!! " + f.toString());
+    
+    if (correctAnswer.toString() === answer.toString()) {
+      alert('Woowow ' + correctAnswer +" is correct, genius !!! " + f.toString());
+      
+    }
     else alert ('Sorry, that is not correct');  
     
   };
+  
+  const gameLevelHandler = (n)=> {
+    
+    if (n <= 2) return 1;
+    let f = [0, 1, 1];
+    for(let i = 3; i <= n; i++) {
+      f[i] = f[i-1] + f[i-2];
+    }
+    let correctAnswer = f[f.length-1];
+    let counter = 1
+    
+    if (correctAnswer.toString() === answer.toString()) {
+      let val = parseInt( nValue)  + parseInt( counter)
+      return val;
+    }
+    else {
+      return parseInt(nValue)
+    }
+  }
   
   const nValueOnHandleChange = e => {
     let valu = e.target.value;
     if (!Number(valu)) {
       return;
     }
-    nValueHandler(e.target.value);
+    setnValue(e.target.value);
   };
   
   const answerOnHandleChange = e => {
@@ -34,7 +56,7 @@ function App() {
     if (!Number(valu)) {
       return;
     }
-    answerHandler(e.target.value);
+    setAnswer(e.target.value);
   };
   
   return (
@@ -48,6 +70,8 @@ function App() {
     value={nValue}
     onChange={e=>nValueOnHandleChange(e)}
     type='number'
+    min='3'
+    max={gameLevelHandler(nValue)}
     
     ></input>
     
@@ -58,6 +82,7 @@ function App() {
     value={answer}
     onChange={e=>answerOnHandleChange(e)}      
     type="number"
+    min='2'
     
     ></input>
     
